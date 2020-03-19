19c42d73b3f64c48fa20bfe75446da0608076930
Kalana Dhanajaya <kalanad@eng.pdn.ac.lk>
2020-03-19 19:06:59 +0530

    initial project architecture

---
+++ frontend/src/main/java/com/github/debs/frontend/controller/con.java
@@ -0,0 +1,10 @@
+package com.github.debs.frontend.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class con {
+
+}
